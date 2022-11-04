4d631295a9e5b24991da02261dca41c6238890c8
player01osu <player01osu@gmail.com>
2022-11-04 12:55:05 -0400

    Alert box

--- app.js
+++ app.js
@@ -1,4 +1,33 @@
+setText("name","my_project");
+setText("bpm", "120");
+
+function alert(head, sub) {
+    showElement("alert_head");
+    showElement("alert_sub");
+    showElement("alert_border");
+    setText("alert_head", head);
+    setText("alert_sub", sub);
+
+    setTimeout(function() {
+        hideElement("alert_head");
+        hideElement("alert_sub");
+        setProperty("alert_border", "hidden", true);
+    }, 3000);
+
+}
+
 onEvent("start", "click", function() {
+    // Both name and bpm fields must be filled
+    if (!getText("name")) {
+        alert("Warning!", "Must have project name");
+        return;
+    }
+
+    if (!getText("bpm")) {
+        alert("Warning!", "Must have bpm set (default: 120)");
+        return;
+    }
+
     setScreen("timeline");
 });
 
@@ -37,6 +66,7 @@ onEvent("effects_bbb", "click", function() {
 onEvent("filters_bbb", "click", function() {
     setScreen("filters");
 });
+
 
 
 // vim:expandtab:softtabstop=4:tabstop=4:shiftwidth=4

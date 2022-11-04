69fac8dcecb53ad3c00e4d5bd561c72a0f7d379d
player01osu <player01osu@gmail.com>
2022-11-04 11:57:10 -0400

    Buttons to change screens.

--- app.js
+++ app.js
@@ -1,6 +1,42 @@
-button("id", "text");
-onEvent("id", "click", function( ) {
-
+onEvent("start", "click", function() {
+    setScreen("timeline");
 });
+
+onEvent("timeline_b", "click", function() {
+    setScreen("timeline");
+});
+
+onEvent("effects_b", "click", function() {
+    setScreen("effects");
+});
+
+onEvent("filters_b", "click", function() {
+    setScreen("filters");
+});
+
+onEvent("timeline_bb", "click", function() {
+    setScreen("timeline");
+});
+
+onEvent("effects_bb", "click", function() {
+    setScreen("effects");
+});
+
+onEvent("filters_bb", "click", function() {
+    setScreen("filters");
+});
+
+onEvent("timeline_bbb", "click", function() {
+    setScreen("timeline");
+});
+
+onEvent("effects_bbb", "click", function() {
+    setScreen("effects");
+});
+
+onEvent("filters_bbb", "click", function() {
+    setScreen("filters");
+});
+
 
 // vim:expandtab:softtabstop=4:tabstop=4:shiftwidth=4

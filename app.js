462d6281852065f165677df76c45907ea0748505
player01osu <player01osu@gmail.com>
2022-11-05 23:33:01 -0400

    Unfunctional audio tab.

--- app.js
+++ app.js
@@ -424,16 +424,58 @@ onEvent("metronome_button", "click", function() {
 
 // vim:expandtab:softtabstop=4:tabstop=4:shiftwidth=4
 var audio = {
-	master_audio: 100,
-	snare: 90,
-	kick: 60,
-	hihat: 70
+	master_audio: 100 | 0,
+	snare: 90 | 0,
+	kick: 60 | 0,
+	hihat: 70 | 0
 };
 
 function sound_panel_tab(screen_name) {
-	//var tabs = screen_name.state.tabs;
-	//tabs_clear(tabs);
+    var tab_idx = 0 | 0;
+    for (; tab_idx < screen.timeline.state.tabs.tab.length; ++tab_idx) {
+        if (screen.timeline.state.tabs.tab[tab_idx].name === "Sound Panel") {
+            break;
+        }
+    }
 
+    var tab = screen.timeline.state.tabs.tab[tab_idx];
+
+	if (tab.loaded) {
+		tabs_show(tab);
+	} else {
+		if (screen_name.id === "timeline") {
+			setProperty("audio_master_timeline_label", "hidden", false);
+			setProperty("audio_master_timeline_slider", "hidden", false);
+
+			setProperty("audio_snare_label", "hidden", false);
+			setProperty("audio_snare_slider", "hidden", false);
+
+			setProperty("audio_kick_label", "hidden", false);
+			setProperty("audio_kick_slider", "hidden", false);
+
+			setProperty("audio_hihat_label", "hidden", false);
+			setProperty("audio_hihat_slider", "hidden", false);
+
+			tabs_insert_element(tab, "audio_master_timeline_slider");
+			tabs_insert_element(tab, "audio_master_timeline_label");
+
+			tabs_insert_element(tab, "audio_snare_slider");
+			tabs_insert_element(tab, "audio_snare_label");
+
+			tabs_insert_element(tab, "audio_kick_slider");
+			tabs_insert_element(tab, "audio_kick_label");
+
+			tabs_insert_element(tab, "audio_hihat_slider");
+			tabs_insert_element(tab, "audio_hihat_label");
+
+			setProperty("audio_master_timeline_slider", "value", audio.master_audio);
+			setProperty("audio_snare_slider", "value", audio.snare);
+			setProperty("audio_kick_slider", "value", audio.kick);
+			setProperty("audio_hihat_slider", "value", audio.hihat);
+
+			tab.loaded = true;
+		}
+	}
 }
 var playback = {
     toggle: false,
@@ -592,7 +634,6 @@ function tabs_clear(tab) {
             hideElement(id);
         });
     }
-    //tabs.elements_id = [];
 }
 
 function tabs_show(tab) {

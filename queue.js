f07c01095638b37ccd16a21b11f4b3d47decc2cc
player01osu <player01osu@gmail.com>
2022-11-06 20:33:36 -0500

    Queue implimentation

---
+++ queue.js
@@ -0,0 +1,31 @@
+
+function queue_new() {
+    var queue = {
+        elements: {},
+        head: 0,
+        tail: 0
+    };
+    return queue;
+}
+
+function queue_enqueue(queue, element) {
+    queue.elements[queue.tail] = element;
+    queue.tail++;
+}
+
+function queue_dequeue(queue) {
+    var element = queue.elements[queue.head];
+    delete queue.elements[queue.head];
+    queue.head++;
+    return element;
+}
+
+function queue_length(queue) {
+    return queue.tail - queue.head;
+}
+
+function queue_is_empty(queue) {
+    return queue_length(queue) === 0;
+}
+
+// vim:expandtab:softtabstop=4:tabstop=4:shiftwidth=4
